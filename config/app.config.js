4d6d4f063f0226828b962afbeb58a6d0232284db
Chinepoh Divine-favour <divinechinepoh@gmail.com>
2023-06-21 18:42:59 +0100

    bakendServerUp

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config;
 
 const MONGO_DB_CONFIG = {
-  DB: "mongodb+srv://divinechinepoh:33ZOv7E87JVHjzZE@cluster0.csaostk.mongodb.net/backend?retryWrites=true&w=majority",
+  DB: "mongodb+srv://divinechinepoh:33ZOv7E87JVHjzZE@cluster0.csaostk.mongodb.net/ReactBackendProject?retryWrites=true&w=majority",
 };
 
 module.exports = {
